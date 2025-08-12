487a91b210c79f96a5066630e45c988b1f4cb1e3
Asil2003 <asilalabri03@gmail.com>
2025-08-12 11:50:13 +0400

    feat(auth): add basic AuthController skeleton

---
+++ src/main/java/com/inventory/auth/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.inventory.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping("/auth/hello")
+    public String hello() {
+        return "Hello from AuthController!";
+    }
+}
