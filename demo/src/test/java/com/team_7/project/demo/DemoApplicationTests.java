54c93898a3e56f8cf5009d72c3993f290b581c3c
AsmaAlabri <aa8454854@gmail.com>
2025-08-12 15:09:18 +0400

    Add files via upload

---
+++ demo/src/test/java/com/team_7/project/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_7.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
