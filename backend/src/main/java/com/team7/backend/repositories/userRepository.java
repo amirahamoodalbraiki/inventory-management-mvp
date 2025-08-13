8321f6eff20e2149f803e6d378cc57ee2dcbfadf
mohammed <mohammed@domain.com>
2025-08-13 17:29:33 +0400

    added user entiity, controller, repastory. Also configured in-memory local database

--- backend/src/main/java/com/team7/backend/repositories/userRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team7.backend.repositories;
-
-import com.team7.backend.entities.user;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface userRepository  extends JpaRepository<Integer, user> {
-}
