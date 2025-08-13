601fdf1914d2aeb7ceccbdc3a7728a1dfb51c5c3
Asil2003 <asilalabri03@gmail.com>
2025-08-13 16:25:19 +0400

    Create AuthService.java

---
+++ backend/src/main/java/com/inventory/auth/service/AuthService.java
@@ -0,0 +1,16 @@
+package com.inventory.auth.service;
+
+import com.inventory.auth.model.User;
+
+public class AuthService {
+    
+    public boolean login(String email, String password) {
+        // TODO: implement login logic
+        return false;
+    }
+
+    public User register(User user) {
+        // TODO: implement registration logic
+        return user;
+    }
+}
