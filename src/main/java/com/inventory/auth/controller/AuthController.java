84134a9f3ad9c86986fc1b618ba090728d051afc
Asil2003 <asilalabri03@gmail.com>
2025-08-14 19:18:36 +0400

    Update AuthController.java

--- src/main/java/com/inventory/auth/controller/AuthController.java
+++ src/main/java/com/inventory/auth/controller/AuthController.java
@@ -1,4 +1,4 @@
-package com.inventory.auth.controller;
+package com.example.inventory.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
