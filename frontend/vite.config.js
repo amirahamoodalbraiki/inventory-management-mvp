f4f66d9b4288c82686576823409397036dbe897b
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2025-08-21 02:47:57 +0400

    added correct url for notification endpoint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,6 +22,10 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
+      '/notifications': { 
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
     },
   },
 })
