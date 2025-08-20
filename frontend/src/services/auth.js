7f8bc5b742208058d1eb5e2c780543c078808f90
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2025-08-21 01:10:54 +0400

    added function to decode the token and extract userId

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -12,3 +12,15 @@ export function getUserRole() {
     return null;
   }
 }
+
+export function getUserId() {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+  try {
+    const decoded = jwtDecode(token);
+    return decoded.userId || null;
+  } catch (err) {
+    console.error("Invalid token:", err);
+    return null;
+  }
+}
