9c9900190809f0a68dc4ec7c74a2704c4f43b7dc
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2025-08-21 14:29:23 +0400

    deployment

--- backend/src/main/java/com/team7/backend/security/CorsConfig.java
+++ backend/src/main/java/com/team7/backend/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-          .allowedOrigins("http://localhost:5173/") // React dev server
+          .allowedOrigins("https://inventory-management-mvp.vercel.app") // React dev server
           .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
           .allowedHeaders("*")
           .allowCredentials(true);
