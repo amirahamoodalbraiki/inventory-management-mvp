8a02b31a56709f1b024cb3b89524ae03a7c0307d
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2025-08-22 15:38:37 +0400

    deleted unused alert files, already implemented with notification files

--- backend/src/main/java/com/team7/backend/repositories/AlertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team7.backend.repositories;
-
-import com.team7.backend.entities.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertRepository extends JpaRepository<Alert, Integer> {
-}
